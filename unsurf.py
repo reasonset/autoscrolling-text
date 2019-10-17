cea05e9c6ca0a215f1efae7d0359094fe8d6df6f
MASAKI Haruka <yek@reasonset.net>
2019-10-18 06:25:29 +0900

    Unsurf: icon support.

--- unsurf.py
+++ unsurf.py
@@ -3,6 +3,7 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtGui import QIcon
 import re
 import os
 
@@ -16,13 +17,20 @@ if (argc != 2):
 # For opening local file.
 url = argv[1]
 if re.match('^[a-z]+://', url):
-  pass # do nothing.
+    pass # do nothing.
 elif re.match('^/', url):
-  url = 'file://' + url
+    url = 'file://' + url
 else:
-  url = "file://" + os.getcwd() + "/" + url
+    url = "file://" + os.getcwd() + "/" + url
 
 app = QApplication([])
+
+# Choice application icon. I don't know smart way for choice generic web browser icon.
+for iconpath in ["Papirus/64x64/apps/redhat-web-browser.svg", "Vibrancy-Colors/apps/96/browser.png", "Papirus/64x64/apps/internet-web-browser.svg", "breeze/apps/48/plasma-browser-integration.svg", "breeze/apps/48/internet-web-browser.svg", "Adwaita/scalable/apps/web-browser-symbolic.svg", "gnome/256x256/apps/web-browser.png", "ePapirus/22x22/actions/web-browser.svg", "AwOken/clear/128x128/apps/browser.png", "andromeda/apps/48/internet-web-browser.svg"]:
+    if os.path.exists("/usr/share/icons/" + iconpath):
+        app.setWindowIcon(QIcon.fromTheme("web-browser", QIcon("/usr/share/icons/" + iconpath)))
+        break
+
 web = QWebEngineView()
 web.load(QUrl(url))
 web.show()
