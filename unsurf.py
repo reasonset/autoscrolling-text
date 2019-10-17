558fa5f2d491d17c7d22b4a412c2cb86a8f177ce
MASAKI Haruka <yek@reasonset.net>
2019-10-18 05:36:09 +0900

    Support opening local file with unsurf.py

--- unsurf.py
+++ unsurf.py
@@ -3,6 +3,8 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWebEngineWidgets import *
+import re
+import os
 
 argv = sys.argv
 argc = len(argv)
@@ -11,9 +13,18 @@ if (argc != 2):
     print("Usage:\n unsurf.py addr")
     quit()
 
+# For opening local file.
+url = argv[1]
+if re.match('^[a-z]+://', url):
+  pass # do nothing.
+elif re.match('^/', url):
+  url = 'file://' + url
+else:
+  url = "file://" + os.getcwd() + "/" + url
+
 app = QApplication([])
 web = QWebEngineView()
-web.load(QUrl(argv[1]))
+web.load(QUrl(url))
 web.show()
 
 sys.exit(app.exec_())
