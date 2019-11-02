f8ec7600d3142b009d3095ac8fc90b2c110850c6
MASAKI Haruka <yek@reasonset.net>
2019-11-02 13:29:33 +0900

    To offtherecord browsing.

--- unsurf.py
+++ unsurf.py
@@ -23,18 +23,23 @@ elif re.match('^/', url):
 else:
     url = "file://" + os.getcwd() + "/" + url
 
-app = QApplication([])
-
+app = QApplication(["unsurf"])
 
 # Choice application icon. I don't know smart way for choice generic web browser icon.
 for iconpath in ["Papirus/64x64/apps/redhat-web-browser.svg", "Vibrancy-Colors/apps/96/browser.png", "Papirus/64x64/apps/internet-web-browser.svg", "breeze/apps/48/plasma-browser-integration.svg", "breeze/apps/48/internet-web-browser.svg", "Adwaita/scalable/apps/web-browser-symbolic.svg", "gnome/256x256/apps/web-browser.png", "ePapirus/22x22/actions/web-browser.svg", "AwOken/clear/128x128/apps/browser.png", "andromeda/apps/48/internet-web-browser.svg"]:
     if os.path.exists("/usr/share/icons/" + iconpath):
         app.setWindowIcon(QIcon.fromTheme("web-browser", QIcon("/usr/share/icons/" + iconpath)))
         break
-
 web = QWebEngineView()
+
+# Set profile name if you need.
+pf = QWebEngineProfile()
+page = QWebEnginePage(pf, web)
+web.setPage(page)
+
 web.setWindowTitle("Unsruf Quick WebBrowser")
 web.load(QUrl(url))
 web.show()
 
 sys.exit(app.exec_())
+
